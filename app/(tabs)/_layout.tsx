511db06a97140f2afe4e6063e3ee8327cc62192e
Kingbonwa1994 <bonwamdluli@gmail.com>
2024-05-26 16:27:44 +0200

    icons set up

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
+import { Link, Stack, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
 
 import Colors from '@/constants/Colors';
@@ -19,18 +19,16 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
+    <Stack
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
       }}>
-      <Tabs.Screen
+      <Stack.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Lumedo',
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -47,13 +45,12 @@ export default function TabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
+      <Stack.Screen
         name="two"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'To see',
         }}
       />
-    </Tabs>
+    </Stack>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,11 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import { Image, StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Image width={40} height={40} src='/assets/images/bulb.jpg' />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
@@ -16,8 +13,10 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    textShadowColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#000000',
   },
   title: {
     fontSize: 20,
@@ -25,7 +24,9 @@ const styles = StyleSheet.create({
   },
   separator: {
     marginVertical: 30,
-    height: 1,
+    marginLeft: 100,
+    height: 60,
+    borderRadius: 20,
     width: '80%',
   },
 });
